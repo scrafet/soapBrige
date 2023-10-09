66724888cffb8ec73bd45fabcaf4a6294074db91
scrafet <scrafet@gmail.com>
2023-10-08 20:54:25 -0500

    Cuarto Commit - lib suds - devuelve Datos

--- main.py
+++ main.py
@@ -9,33 +9,32 @@ sessionId = response
 print(sessionId)
 
 # Obtiene el tipo de consulta
-intOpcion = input("Ingrese tipo de consulta")
+intOpcion = int(input("Ingrese tipo de consulta : "))
 
 # Obtiene el tipo de documento del afiliado.
-tipoDocumento = input("Ingrese tipo de documento")
+tipoDocumento = str(input("Ingrese tipo de documento : "))
 
 # Obtiene el número de documento del afiliado.
-nroDocumento = input("Ingrese el dni")
+nroDocumento = str(input("Ingrese el dni : "))
 
 #  número de documento de seguridad.
-Dni = 44790320
+Dni = "44790320"
 
 # Llama al método ConsultarAfiliadoFuaE.
-responseAfiliado = client.service.ConsultarAfiliadoFuaE(intOpcion, sessionId, Dni, nroDocumento)
+responseAfiliado = client.service.ConsultarAfiliadoFuaE(intOpcion, sessionId, Dni, tipoDocumento, nroDocumento)
 
 # Obtiene el afiliado de la respuesta.
 afiliado = responseAfiliado
 
-
-
-print('This statement is always executed')
-
-
-if afiliado.IdError=0:
-    # block of code if condition is True
+# Si el afiliado no existe, se imprime el mensaje de error.
+# if afiliado.IdError != 0:
+#   print(afiliado.Resultado)
+# else:
+# Imprime los datos del afiliado.
+#   print(afiliado)
+# Si el afiliado no existe, se imprime el mensaje de error.
+if afiliado is None:
+    print(afiliado.Resultado)
+else:
     # Imprime los datos del afiliado.
     print(afiliado)
-
-else:
-    # block of code if condition is False
-    print(afiliado.Resultado)
