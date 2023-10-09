ff48e1a9b5b399a151e86b608844c0c4434701e3
scrafet <scrafet@gmail.com>
2023-10-08 20:55:40 -0500

    Quinto Commit - agregado modelo

---
+++ Models.py
@@ -0,0 +1,39 @@
+class Afiliado:
+    def __init__(self, id_error, resultado, tipo_documento, nro_documento,
+                 ape_paterno, ape_materno, nombres,
+                 fec_afiliacion, eess, desc_eess, eess_ubigeo,
+                 desc_eess_ubigeo, regimen, tipo_seguro, desc_tipo_seguro,
+                 contrato, fec_caducidad, estado, tabla, id_num_reg,
+                 genero, fec_nacimiento, id_ubigeo, disa,
+                 tipo_formato, nro_contrato, correlativo, id_plan,
+                 id_grupo_poblacional, msg_confidencial):
+        self.id_error = id_error
+        self.resultado = resultado
+        self.tipo_documento = tipo_documento
+        self.nro_documento = nro_documento
+        self.ape_paterno = ape_paterno
+        self.ape_materno = ape_materno
+        self.nombres = nombres
+        self.fec_afiliacion = fec_afiliacion
+        self.eess = eess
+        self.desc_eess = desc_eess
+        self.eess_ubigeo = eess_ubigeo
+        self.desc_eess_ubigeo = desc_eess_ubigeo
+        self.regimen = regimen
+        self.tipo_seguro = tipo_seguro
+        self.desc_tipo_seguro = desc_tipo_seguro
+        self.contrato = contrato
+        self.fec_caducidad = fec_caducidad
+        self.estado = estado
+        self.tabla = tabla
+        self.id_num_reg = id_num_reg
+        self.genero = genero
+        self.fec_nacimiento = fec_nacimiento
+        self.id_ubigeo = id_ubigeo
+        self.disa = disa
+        self.tipo_formato = tipo_formato
+        self.nro_contrato = nro_contrato
+        self.correlativo = correlativo
+        self.id_plan = id_plan
+        self.id_grupo_poblacional = id_grupo_poblacional
+        self.msg_confidencial = msg_confidencial
